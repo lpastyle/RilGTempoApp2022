c3e5b40a49c239d7553407d835da3ed159b0e929
lpastyle <lpastorelli@lpastyle.com>
2022-02-01 15:10:50 +0100

    added getTempoDaysColor api definition

--- app/src/main/java/com/example/rilgtempoapp2022/IEdfApi.java
+++ app/src/main/java/com/example/rilgtempoapp2022/IEdfApi.java
@@ -11,5 +11,10 @@ public interface IEdfApi {
             @retrofit2.http.Query("TypeAlerte") String alertType
     );
 
-
+    // https://particulier.edf.fr/bin/edf_rc/servlets/ejptemponew?Date_a_remonter=2021-01-26&TypeAlerte=TEMPO
+    @GET("bin/edf_rc/servlets/ejptemponew")
+    Call<TempoDaysColor> getTempoDaysColor(
+            @retrofit2.http.Query("Date_a_remonter") String date,
+            @retrofit2.http.Query("TypeAlerte") String alertType
+    );
 }
