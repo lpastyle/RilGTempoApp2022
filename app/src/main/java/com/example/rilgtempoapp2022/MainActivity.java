f4727b4522446b2b61ed97f94d02996db77450cc
lpastyle <lpastorelli@lpastyle.com>
2022-02-02 09:35:26 +0100

    update nb days

--- app/src/main/java/com/example/rilgtempoapp2022/MainActivity.java
+++ app/src/main/java/com/example/rilgtempoapp2022/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.net.HttpURLConnection;
 
@@ -17,10 +18,21 @@ public class MainActivity extends AppCompatActivity {
     public static String LOG_TAG = MainActivity.class.toString();
     public static IEdfApi edfApi = null;
 
+    // views
+    private TextView redDaysTv;
+    private TextView whiteDaysTv;
+    private TextView blueDaysTv;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // init views
+        redDaysTv = findViewById(R.id.red_days_tv);
+        whiteDaysTv = findViewById(R.id.white_days_tv);
+        blueDaysTv = findViewById(R.id.blue_days_tv);
 
         // init Retrofit client
         Retrofit retrofitClient = ApiClient.get();
@@ -44,6 +56,9 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(LOG_TAG,"nb red days = " + response.body().getParamNbJRouge());
                         Log.d(LOG_TAG,"nb white days = " + response.body().getParamNbJBlanc());
                         Log.d(LOG_TAG,"nb blue days = " + response.body().getParamNbJBleu());
+                        redDaysTv.setText(String.valueOf(response.body().getParamNbJRouge()));
+                        whiteDaysTv.setText(String.valueOf(response.body().getParamNbJBlanc()));
+                        blueDaysTv.setText(String.valueOf(response.body().getParamNbJBleu()));
                     }
                 }
 
